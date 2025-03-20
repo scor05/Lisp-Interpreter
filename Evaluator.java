04a8add58ac671d42080b57d3029f9152adb11c2
Samuuu132 <gud24451@uvg.edu.gt>
2025-03-20 00:17:25 -0600

    creacion de lo metodos de menor que,  el cond y el de evaluar la funcion

--- Evaluator.java
+++ Evaluator.java
@@ -54,7 +54,7 @@ public class Evaluator {
             return expression;
         }
     }
-}
+
     private Object defineFunction(List<?> list, Environment env) {
         String name = list.get(1).toString();
         @SuppressWarnings("unchecked")
@@ -96,3 +96,39 @@ public class Evaluator {
         }
         throw new RuntimeException("No se puede comparar valores con datos no numericos.");
     }
+     private Object lessThan(Object expr1, Object expr2, Environment env) {
+        Object val1 = eval(expr1, env);
+        Object val2 = eval(expr2, env);
+        if (val1 instanceof Number && val2 instanceof Number) {
+            return ((Number) val1).doubleValue() < ((Number) val2).doubleValue();
+        }
+        throw new RuntimeException("No se puede comparar valores no numericos.");
+    }
+
+    private Object evaluateCond(List<?> conditions, Environment env) {
+        for (Object condition : conditions) {
+            List<?> cond = (List<?>) condition;
+            Object test = eval(cond.get(0), env);
+            if (test instanceof Boolean && (Boolean) test) {
+                return eval(cond.get(1), env);
+            }
+        }
+        return null;
+    }
+
+    private Object funcall(Object function, Object arg, Environment env) {
+        LispFunction func = env.getFunction(function.toString());
+        List<Object> args = new ArrayList<>();
+        args.add(arg);
+        return func.apply(args, env);
+    }
+
+    private Object evaluateFunction(String operator, List<?> args, Environment env) {
+        LispFunction function = env.getFunction(operator);
+        List<Object> evaluatedArgs = new ArrayList<>();
+        for (Object arg : args) {
+            evaluatedArgs.add(eval(arg, env));
+        }
+        return function.apply(evaluatedArgs, env);
+    }
+}
